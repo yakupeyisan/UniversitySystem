fa2da028bd8afcbd7b70385b9de1a824ea4efbc2
Yakup Eyisan <47880402+yakupeyisan@users.noreply.github.com>
2025-10-25 19:28:40 +0300

    Update

--- src/Modules/PersonMgmt/Application/DTOs/UpdatePersonRequest.cs
+++ src/Modules/PersonMgmt/Application/DTOs/UpdatePersonRequest.cs
@@ -1,8 +1,8 @@
 namespace PersonMgmt.Application.DTOs;
 public class UpdatePersonRequest
 {
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
     public Guid? DepartmentId { get; set; }
     public string? ProfilePhotoUrl { get; set; }
 }
